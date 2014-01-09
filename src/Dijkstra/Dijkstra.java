78aff1c301d714747a811cb7401059705e8c55b7
Yveml <ym.lefloch@gmail.com>
2014-01-09 22:59:15 +0100

    modif de l'accent

--- src/Dijkstra/Dijkstra.java
+++ src/Dijkstra/Dijkstra.java
@@ -39,6 +39,7 @@ public class Dijkstra {
 
 					if (pi.getPoids(pivot) + gi.getPoids(pivot, x) < pi
 							.getPoids(x)) {
+						System.out.println(Integer.toString(pi.getPoids(pivot) + gi.getPoids(pivot, x)));
 						pi.setPoids(x,
 								pi.getPoids(pivot) + gi.getPoids(pivot, x));
 
@@ -63,7 +64,7 @@ public class Dijkstra {
 				count++;
 			}
 			if(count==2){
-				System.out.println("Je suis coince à tout jamais dans ce labyrinthe la!");
+				System.out.println("Je suis coince a tout jamais dans ce labyrinthe la!");
 				done_lose=true;
 			}
 		}
